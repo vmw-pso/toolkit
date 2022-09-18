b07447df119c8285754d56998372c1a5c06b9962
vmw-pso <pstacey@vmware.com>
2022-09-18 07:03:45 -0400

    Added reading JSON and sending error as JSON

--- tools.go
+++ tools.go
@@ -14,6 +14,12 @@ type Tools struct {
 	AllowUnknownFields bool
 }
 
+type JSONResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
 func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data any) error {
 	maxBytes := 1024 * 1024 // 1 MB
 	if t.MaxJSONSize > 0 {
@@ -54,6 +60,41 @@ func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data any) error
 
 	if err := decoder.Decode(&struct{}{}); err != io.EOF {
 		return errors.New("body must contain only one JSON value")
+	}
+	return nil
+}
+
+func (t *Tools) ErrorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload JSONResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return t.WriteJSON(w, statusCode, payload)
+}
+
+func (t *Tools) WriteJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	output, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(output)
+	if err != nil {
+		return err
 	}
 	return nil
 }
